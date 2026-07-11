6d313753fc00f20cd07378e73469703b1f70ec4c
aabbaspour1103-alt <a.abbaspour1103@gmail.com>
2026-07-11 22:02:20 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,4 +1,21 @@
-- name: Run bot
-  env:
-    TOKEN: ${{ secrets.TOKEN }}
-  run: python bot.py
+import os
+import requests
+
+TOKEN = os.getenv("TOKEN")
+CHANNEL_ID = "@CryptoBrew"
+
+print("TOKEN:", TOKEN)
+
+if not TOKEN:
+    raise Exception("TOKEN is None. GitHub Secret تنظیم نشده است.")
+
+url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
+
+data = {
+    "chat_id": CHANNEL_ID,
+    "text": "✅ ربات با موفقیت از GitHub Actions اجرا شد."
+}
+
+response = requests.post(url, data=data)
+
+print(response.text)
