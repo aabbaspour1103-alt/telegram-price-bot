f1f45fd7678f40ecd3833e47230a4cb041f83f3b
aabbaspour1103-alt <a.abbaspour1103@gmail.com>
2026-07-11 17:49:36 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,37 @@
+import os
+import requests
 
+TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
+CHANNEL = "@YOUR_CHANNEL_ID"
+
+message = """
+💰 قیمت لحظه‌ای بازار
+
+💵 دلار آمریکا:
+💶 یورو اروپا:
+💷 پوند انگلیس:
+🇨🇳 یوان چین:
+🇦🇪 درهم امارات:
+🇹🇷 لیر ترکیه:
+
+🥇 اونس جهانی طلا:
+🥇 طلای ۱۸ عیار:
+🥇 طلای ۲۴ عیار:
+
+₿ بیت‌کوین:
+🔷 اتریوم:
+◎ سولانا:
+✖️ ریپل:
+"""
+
+url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
+
+response = requests.post(
+    url,
+    data={
+        "chat_id": CHANNEL,
+        "text": message
+    }
+)
+
+print(response.text)
