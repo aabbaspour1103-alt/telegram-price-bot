29d5a80a04bb730282dcc5758986c72771ae6435
aabbaspour1103-alt <a.abbaspour1103@gmail.com>
2026-07-11 19:04:54 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,3 +1,35 @@
+import os
+import requests
+import time
 
-TOKEN = os.getenv("8670280483:AAH-QTdk5yJDkk67qmDkkAQ6igeNydVNF8c")
-CHANNEL = "@CryptoBrew"
+TOKEN = os.getenv("TOKEN")
+CHANNEL_ID = "@CryptoBrew"
+
+def send_message(text):
+    url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
+    data = {
+        "chat_id": CHANNEL_ID,
+        "text": text
+    }
+    requests.post(url, data=data)
+
+def get_message():
+    return """
+💰 قیمت لحظه‌ای بازار
+
+💵 دلار آمریکا:
+💶 یورو اروپا:
+💷 پوند انگلیس:
+🇨🇳 یوان چین:
+🇦🇪 درهم امارات:
+🥇 اونس جهانی طلا:
+🥇 طلای ۱۸ عیار:
+🔶 بیت‌کوین (BTC):
+🔷 اتریوم (ETH):
+
+⏰ بروزرسانی هر ۴ ساعت
+"""
+
+while True:
+    send_message(get_message())
+    time.sleep(14400)
