4b5fc441cf760f56a6ba3cbce381c9a550151f8c
aabbaspour1103-alt <a.abbaspour1103@gmail.com>
2026-07-11 21:03:50 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -10,29 +10,8 @@ def send_message(text):
         "chat_id": CHANNEL_ID,
         "text": text
     }
-    response = requests.post(url, data=data, timeout=10)
+
+    response = requests.post(url, data=data)
     print(response.text)
 
-def get_message():
-    return """
-💰 قیمت لحظه‌ای بازار
-
-💵 دلار آمریکا:
-💶 یورو اروپا:
-💷 پوند انگلیس:
-🇨🇳 یوان چین:
-🇦🇪 درهم امارات:
-🥇 اونس جهانی طلا:
-🥇 طلای ۱۸ عیار:
-🔶 بیت‌کوین (BTC):
-🔷 اتریوم (ETH):
-
-⏰ بروزرسانی هر ۴ ساعت
-"""
-
-print("Bot started")
-
-message = get_message()
-send_message(message)
-
-print("Message sent successfully")
+send_message("تست ربات")
