33edb5eaebe19e5547aab2c7f3d1fa3f7eaeac1e
aabbaspour1103-alt <a.abbaspour1103@gmail.com>
2026-07-11 23:40:33 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,16 +1,39 @@
 import os
 import requests
+from datetime import datetime
 
 TOKEN = os.getenv("TOKEN")
 CHANNEL_ID = os.getenv("CHANNEL_ID")
+NAVASAN_API_KEY = os.getenv("NAVASAN_API_KEY")
 
-url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
+def send_message(text):
+    url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
+    requests.post(url, data={
+        "chat_id": CHANNEL_ID,
+        "text": text
+    })
 
-data = {
-    "chat_id": CHANNEL_ID,
-    "text": "✅ تست ربات موفق بود"
-}
+# دریافت قیمت‌ها از نوسان
+url = f"https://api.navasan.tech/latest/?api_key={NAVASAN_API_KEY}"
+data = requests.get(url).json()
 
-response = requests.post(url, data=data)
+usd = data["usd_sell"]["value"]
+eur = data["eur"]["value"]
+gbp = data["gbp"]["value"]
+cny = data["cny"]["value"]
 
-print(response.text)
+message = f"""
+💰 قیمت لحظه‌ای بازار
+
+💵 دلار آمریکا: {usd} تومان
+💶 یورو اروپا: {eur} تومان
+💷 پوند انگلیس: {gbp} تومان
+🇨🇳 یوان چین: {cny} تومان
+
+⏰ بروزرسانی:
+{datetime.now().strftime("%Y-%m-%d %H:%M")}
+"""
+
+send_message(message)
+
+print("Message sent successfully")
