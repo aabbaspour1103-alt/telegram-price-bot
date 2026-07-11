d972bd63668145f683cbc2560958ed977650116f
aabbaspour1103-alt <a.abbaspour1103@gmail.com>
2026-07-11 21:22:50 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,19 +2,19 @@ import os
 import requests
 
 TOKEN = os.getenv("TOKEN")
-CHANNEL_ID = "@CryptoBrew"
+CHANNEL_ID = "-1003901224506"
 
-print("شروع تست")
 print("TOKEN:", TOKEN)
 
-url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
+def send_message():
+    url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
+    
+    data = {
+        "chat_id": CHANNEL_ID,
+        "text": "✅ تست ربات انجام شد\nکانال با شناسه عددی بررسی شد."
+    }
 
-data = {
-    "chat_id": CHANNEL_ID,
-    "text": "تست ربات"
-}
+    response = requests.post(url, data=data)
+    print(response.text)
 
-response = requests.post(url, data=data)
-
-print(response.text)
-print("پایان تست")
+send_message()
