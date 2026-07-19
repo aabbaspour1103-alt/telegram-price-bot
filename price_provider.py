e6262be80b9b3ffc79fdabfcf9e3fea081225b10
aabbaspour1103-alt <a.abbaspour1103@gmail.com>
2026-07-19 20:04:49 +0330

    Update price_provider.py

--- price_provider.py
+++ price_provider.py
@@ -1,22 +1,48 @@
 import requests
 from bs4 import BeautifulSoup
 
+
 URL = "https://www.tgju.org"
 
 HEADERS = {
-    "User-Agent": (
-        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
-        "AppleWebKit/537.36 (KHTML, like Gecko) "
-        "Chrome/137.0 Safari/537.36"
-    )
+    "User-Agent": "Mozilla/5.0"
 }
 
-def get_html():
-    response = requests.get(URL, headers=HEADERS, timeout=20)
-    response.raise_for_status()
-    return BeautifulSoup(response.text, "html.parser")
+
+def test_page():
+
+    response = requests.get(
+        URL,
+        headers=HEADERS,
+        timeout=20
+    )
+
+    soup = BeautifulSoup(
+        response.text,
+        "html.parser"
+    )
+
+    # جستجوی کلمات مهم در صفحه
+    keywords = [
+        "dollar",
+        "price_dollar",
+        "سکه",
+        "دلار",
+        "طلا"
+    ]
+
+    for word in keywords:
+        print("\nSEARCH:", word)
+
+        result = soup.find_all(
+            string=lambda text: text and word in text
+        )
+
+        print("FOUND:", len(result))
+
+        for item in result[:3]:
+            print(item.strip())
 
 
 if __name__ == "__main__":
-    soup = get_html()
-    print(soup.title.text)
+    test_page()
