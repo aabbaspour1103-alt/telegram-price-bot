7043a8f24b41ea35fc489e0150256d2eeb2da846
aabbaspour1103-alt <a.abbaspour1103@gmail.com>
2026-07-19 20:18:25 +0330

    Update price_provider.py

--- price_provider.py
+++ price_provider.py
@@ -11,54 +11,98 @@ HEADERS = {
 
 
 def clean_number(value):
-    return int(
-        re.sub(r"[^\d]", "", value)
-    )
+    try:
+        return int(
+            re.sub(r"[^\d]", "", value)
+        )
+    except:
+        return None
+
 
 
 def get_prices():
 
-    response = requests.get(
-        URL,
-        headers=HEADERS,
-        timeout=20
-    )
-
-    soup = BeautifulSoup(
-        response.text,
-        "html.parser"
-    )
-
-    text = soup.get_text(
-        " ",
-        strip=True
-    )
-
-
     prices = {}
 
-
-    patterns = {
-        "gold18": r"طلا ۱۸\s+([\d,]+)",
-        "mithqal": r"مثقال طلا\s+([\d,]+)",
-        "emami": r"سکه\s+([\d,]+)",
-        "usd": r"دلار\s+([\d,]+)",
-        "usdt": r"تتر\s+([\d,]+)"
-    }
-
-
-    for key, pattern in patterns.items():
-
-        result = re.search(
-            pattern,
-            text
+    try:
+        response = requests.get(
+            URL,
+            headers=HEADERS,
+            timeout=20
         )
 
-        if result:
-            prices[key] = clean_number(
-                result.group(1)
+        response.raise_for_status()
+
+        soup = BeautifulSoup(
+            response.text,
+            "html.parser"
+        )
+
+        text = soup.get_text(
+            " ",
+            strip=True
+        )
+
+
+        patterns = {
+
+            # طلا و سکه
+            "gold18": r"طلا ۱۸\s+([\d,]+)",
+            "mithqal": r"مثقال طلا\s+([\d,]+)",
+            "emami": r"سکه\s+([\d,]+)",
+
+
+            # ارزها
+            "usd": r"دلار\s+([\d,]+)",
+            "eur": r"یورو\s+([\d,]+)",
+            "gbp": r"پوند\s+([\d,]+)",
+            "aed": r"درهم امارات\s+([\d,]+)",
+            "sar": r"ریال عربستان\s+([\d,]+)",
+            "try": r"لیر ترکیه\s+([\d,]+)",
+            "cny": r"یوان چین\s+([\d,]+)",
+
+
+            # ارز دیجیتال
+            "usdt": r"تتر\s+([\d,]+)"
+        }
+
+
+        for key, pattern in patterns.items():
+
+            result = re.search(
+                pattern,
+                text
             )
-        else:
+
+            if result:
+                prices[key] = clean_number(
+                    result.group(1)
+                )
+            else:
+                prices[key] = None
+
+
+    except Exception as e:
+
+        print(
+            "TGJU Error:",
+            e
+        )
+
+        # اگر سایت قطع بود، کل ربات نخوابد
+        for key in [
+            "gold18",
+            "mithqal",
+            "emami",
+            "usd",
+            "eur",
+            "gbp",
+            "aed",
+            "sar",
+            "try",
+            "cny",
+            "usdt"
+        ]:
             prices[key] = None
 
 
